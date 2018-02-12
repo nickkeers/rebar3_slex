e11c28d804c5332a78bb9e02e6f40b70bbe0e7f6
Nicholas Keers <nicholas.keers@bet365.com>
2018-02-12 11:23:24 +0000

    Rename to slex

--- src/rebar3_slex_prv.erl
+++ src/rebar3_slex_prv.erl
@@ -2,7 +2,7 @@
 
 -export([init/1, do/1, format_error/1]).
 
--define(PROVIDER, compile).
+-define(PROVIDER, slex).
 -define(DEPS, [app_discovery]).
 
 %% ===================================================================
